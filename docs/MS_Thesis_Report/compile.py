192555269c537b643166ab4dfb2bf2743564428f
Soham Mukherjee <soham.m@live.in>
2017-04-26 09:36:42 +0530

    Adding updated thesis to master

--- docs/MS_Thesis_Report/compile.py
+++ docs/MS_Thesis_Report/compile.py
@@ -1,5 +1,14 @@
+#===========================================
+# Code to compile pdflatex + bibtex
+# files. To run: 
+# python compile.py <filename without .tex>
+# i.e. python compile.py draft
+#===========================================
+
 #!/usr/bin/python
+
 import subprocess, sys
+
 commands = [
     ['pdflatex', sys.argv[1] + '.tex'],
     ['bibtex', sys.argv[1] + '.aux'],
